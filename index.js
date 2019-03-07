ecb8203df98e9b657e8e4b61bb3cd4ec9358d1d2
Tami Churns <tchurns@paperlesspost.com>
2019-03-07 02:02:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ let newID = (1000000000 + oldID)
 let ageIsValid = Number.isInteger(currentAge)
 let randomNumber = Math.random()
 let randomInteger = Math.round(randomNumber)
-let randomUserID = 
+let randomUserID = (randomInteger )
 
