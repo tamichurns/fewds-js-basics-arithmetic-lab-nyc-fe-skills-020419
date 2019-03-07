254707f3be2a841e6b470f348bccc28eb079ff7e
Tami Churns <tchurns@paperlesspost.com>
2019-03-07 01:52:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ You should NOT try to redefine any of the above variables.
 let newID = (1000000000 + oldID)
 let ageIsValid = Number.isInteger(currentAge)
 let randomNumber = Math.random()
-
+let randomInteger = (randomNumber) 
